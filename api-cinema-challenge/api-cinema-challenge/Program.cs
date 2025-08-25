03e1cdebb184f9640b26310ba1ee845abc1c36c6
Trym Haugan Berger <trym.haugan.berger@experisacademy.no>
2025-08-25 10:13:52 +0200

    base project without JWT completed

--- api-cinema-challenge/api-cinema-challenge/Program.cs
+++ api-cinema-challenge/api-cinema-challenge/Program.cs
@@ -1,11 +1,24 @@
 using api_cinema_challenge.Data;
+using api_cinema_challenge.Endpoints;
+using api_cinema_challenge.Models;
+using api_cinema_challenge.Repository;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<CinemaContext>();
+builder.Services.AddDbContext<CinemaContext>(options => {
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+    options.LogTo(message => Debug.WriteLine(message));
+});
+
+builder.Services.AddScoped(typeof(IRepository<Customer>), typeof(Repo<Customer>));
+builder.Services.AddScoped(typeof(IRepository<Movie>), typeof(Repo<Movie>));
+builder.Services.AddScoped(typeof(IRepository<Screening>), typeof(Repo<Screening>));
+builder.Services.AddScoped(typeof(IRepository<Ticket>), typeof(Repo<Ticket>));
 
 var app = builder.Build();
 
@@ -17,4 +30,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.ConfigureCinemaEndpoint();
 app.Run();
